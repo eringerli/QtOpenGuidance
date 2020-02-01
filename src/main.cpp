db8399588009ba60c4372d9e20ddb47a9f01ce5e
Christian Riggenbach <criggenbach@magahugu.ch>
2020-02-01 18:41:36 +0100

    setHighDpiScaleFactorRoundingPolicy(PassThrough) -> HighDPI improvement

--- src/main.cpp
+++ src/main.cpp
@@ -109,13 +109,10 @@ const std::vector<QString> permissions( {"android.permission.INTERNET",
 int main( int argc, char** argv ) {
 //  // hack to make the app apear without cropped qt3d-widget
 //  QCoreApplication::setAttribute( Qt::AA_DisableHighDpiScaling );
+
   QCoreApplication::setAttribute( Qt::AA_EnableHighDpiScaling );
-
-//  qputenv( "QT_AUTO_SCREEN_SCALE_FACTOR", "1.5" );
-//  qputenv( "QT_SCALE_FACTOR_ROUNDING_POLICY","PassThrough");
-//  qputenv( "QT_SCALE_FACTOR", "1" );
-
   QCoreApplication::setAttribute( Qt::AA_UseHighDpiPixmaps );
+  QGuiApplication::setHighDpiScaleFactorRoundingPolicy( Qt::HighDpiScaleFactorRoundingPolicy::PassThrough );
 //  QCoreApplication::setAttribute( Qt::AA_Use96Dpi );
 
   // make qDebug() more expressive
